0103de3f9e2fabdc9242d9840c0833e7ad7eac57
alireza <mohammadianalireza85149@gmail.com>
2026-06-26 17:40:57 +0330

    first part of users.views: the index and role_handling functions were added and the structur of user_login and registeration is made, but not compeleted.

---
+++ RestBox/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+app_name = 'users'
+urlpatterns = [
+    path('', views.index, name = 'index'), 
+    path('role_handling/', views.role_handling, name='role_handling'),
+    path('registeration/<str:role>/', views.registeration, name = 'registeration'),
+    path('user_login/<str:role>/', views.login, name='user_login'),
+
+]
--- RestBox/users/views.py
+++ RestBox/users/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
 # Create your views here.
+
+def index(request):
+    return render(request, 'users/index.html')
+
+def role_handling(request):
+    role = request.POST.get('role')
+    action= request.POST.get('action')
+
+    if action == 'register':
+        return redirect(reverse('users:registeration' , kwargs={'role':role}))
+    elif action == 'login':
+        return redirect(reverse('users:user_login' , kwargs={'role':role}))
+# the below functions are just sturctures and they should be compeleted later
+def registeration(request, role):
+    if role == 'host':
+        return HttpResponse('this is registeration page for hosts')
+    elif role == 'guest':
+        return HttpResponse('this is registeration page for guest')
+    
+def login(request, role):
+    if role == 'host':
+        return HttpResponse('this is login page for hosts')
+    elif role == 'guest':
+        return HttpResponse('this is login page for guest')
