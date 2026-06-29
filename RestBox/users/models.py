bf4bc7a589502e627f15117b7c766c032173f2e0
mohammad <ashtrymhmd801@gmail.com>
2026-06-29 12:32:07 +0330

    hash password and added database

--- RestBox/users/models.py
+++ RestBox/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+    fullname = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.fullname
--- RestBox/users/views.py
+++ RestBox/users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.urls import reverse
+from django.contrib.auth.hashers import make_password, check_password
+from .models import UserProfile
 # Create your views here.
 
 def index(request):
@@ -16,28 +18,79 @@ def role_handling(request):
         return redirect(reverse('users:user_login' , kwargs={'role':role}))
 # the below functions are just sturctures and they should be compeleted later
 def registeration(request, role):
-    if role == 'host':
-        # return HttpResponse('this is registeration page for hosts')
-        fullname = request.POST.get('fullname')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        return render(request, 'users/registeration.html')
-    elif role == 'guest':
-        # return HttpResponse('this is registeration page for guest')
-        fullname = request.POST.get('fullname')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        return render(request, 'users/registeration.html')
+    error = None
+    if request.method == 'POST':
+        if role == 'host':
+            fullname = request.POST.get('fullname')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            if len(password) < 8:
+                error = "error: The password must be at least eight characters."
+                return render('users/registeration.html', {'error': error, 'role': role})
+            try:
+                hashed_password = make_password(password)
+                user = UserProfile.objects.create(
+                    fullname=fullname,
+                    email=email,
+                    password=hashed_password,
+                    role=role
+                )
+                return redirect(reverse('users:user_login', kwargs={'role':role}))
+
+            except:
+                error = 'this accountwith this email is already exists.'
+                return render(request , 'users/registeration.html', {'error': error, 'role': role})
+
+        elif role == 'guest':
+            fullname = request.POST.get('fullname')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            if len(password) < 8:
+                error = "error: The password must be at least eight characters."
+                return render(request, 'users/registeration.html', {'error': error, 'role': role})
+            try:
+                hashed_password = make_password(password)
+                user = UserProfile.objects.create(
+                    fullname=fullname,
+                    email=email,
+                    password=hashed_password,
+                    role=role
+                )
+                return redirect(reverse('users:user_login', kwargs={'role':role}))
+            except:
+                error = 'this accountwith this email is already exists.'
+                return render(request , 'users/registeration.html', {'error': error , 'role': role})
+        
+    return render(request, 'users/registeration.html')
     
 def login(request, role):
+    error = None
     if role == 'host':
-        # return HttpResponse('this is login page for hosts')
-        fullname = request.POST.get('fullname')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        return render(request, 'users/login.html')
+        try:
+            user = UserProfile.objects.get(email=email, role=role)
+            if check_password(password, user.password):
+                return redirect('users:index')
+            else:
+                error = "error: The password is wrong"
+        except UserProfile.DoesNotExist:
+            error = "No user with these specifications was found."
+            
+        return render(request, 'users/login.html', {'error': error, 'role' : role})
+            
     elif role == 'guest':
-        # return HttpResponse('this is login page for guest')
         email = request.POST.get('email')
         password = request.POST.get('password')
-        return render(request, 'users/registeration.html')
+        try:
+            user = UserProfile.objects.get(email=email, role=role)
+            if check_password(password, user.password):
+                return redirect('user:index')
+            else:
+                error = "error: The password is wrong"
+        except:
+            error = "error: No user with these specifications was found."
+        
+        return render(request, 'users/login.html', {'error': error, 'role': role})
+    
+    return render(request, 'users/login.html')
