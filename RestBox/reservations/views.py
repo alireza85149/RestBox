3cdd18293422bb77ab1487a72cb1cb293589eb9f
alireza <mohammadianalireza85149@gmail.com>
2026-07-15 09:57:13 +0330

    the search bar is added and it still searches just by city

--- RestBox/reservations/views.py
+++ RestBox/reservations/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from .models import Reservation
+from Villas.models import Villa
 
 # Create your views here.
 def guest_dashboard(request):
     guest_id = request.session.get('user_id')
     reservs = Reservation.objects.filter(guest_id = guest_id)
-    return render(request, 'reservations/guest_dashboard.html', {
-        'reservs': reservs,
-        'count' : reservs.count()
-    })
+    s_city = request.GET.get('city')
+    if s_city:
+        s_check_in = request.GET.get('check_in')
+        s_check_out = request.GET.get('check_out')
+        searched_villas = Villa.objects.filter(city__icontains = s_city)
+        return render(request, 'reservations/guest_dashboard.html', {
+            'reservs': reservs, 
+            'count': reservs.count(),
+            'searched_villas' : searched_villas
+        })
+    else:
+        return render(request, 'reservations/guest_dashboard.html', {
+            'reservs': reservs,
+            'count' : reservs.count(),
+            'searched_villas': Villa.objects.all()
+        })
