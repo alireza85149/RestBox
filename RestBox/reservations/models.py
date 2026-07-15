cdb675ef7a3531ab7e1baf8185548fee110a6151
mohammad <ashtrymhmd801@gmail.com>
2026-07-15 15:29:40 +0330

    fix a tiny problem

--- RestBox/reservations/models.py
+++ RestBox/reservations/models.py
@@ -3,19 +3,18 @@ from users.models import UserProfile
 from Villas.models import Villa
 # Create your models here.
 class Reservation(models.Model):
-    STATUS_CHOICES = [
-        ('pending', 'Pending'),
-        ('accepted', 'Accepted'),
-        ('rejected', 'Rejected'),
-        ('cancelled', 'Cancelled'),
-    ]
     reservation_id = models.AutoField(primary_key=True)
     guest = models.ForeignKey(UserProfile,on_delete=models.CASCADE,related_name='reservations')
     villa = models.ForeignKey(Villa,on_delete=models.CASCADE,related_name='reservations')
     check_in = models.DateField()
     check_out = models.DateField()
     total_price = models.IntegerField()
-    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='pending')
+    status = models.CharField(max_length=10,choices=[
+        ('pending', 'Pending'),
+        ('accepted', 'Accepted'),
+        ('rejected', 'Rejected'),
+        ('cancelled', 'Cancelled'),
+    ],default='pending')
     created_at = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return f"Reservation #{self.reservation_id} - {self.guest.name} - {self.villa.Title}"
