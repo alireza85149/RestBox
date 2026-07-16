9f54c21084294ccff5ee22a1153773b758727c6a
mohammad <ashtrymhmd801@gmail.com>
2026-07-16 14:29:29 +0330

    make commiuncation between availablity and Villas apps in host side and fix some syntax error

--- RestBox/availability/views.py
+++ RestBox/availability/views.py
@@ -8,13 +8,14 @@ def availability(request, villa_id):
     dates = villa.availabilities.all().order_by("date")
     return render(request, 'availability/availability.html', {'villa': villa, 'dates': dates})
 def update_availability(request, villa_id):
-    if villa.host_id != request.session.get('user_id'):
+    j_days = {1: 31, 2: 31, 3: 31, 4: 31, 5: 31, 6: 31, 7: 30, 8: 30, 9: 30, 10: 30, 11: 30, 12: 29}
+    villa = get_object_or_404(Villa, villa_id=villa_id)
+    if villa.host_id_id  != request.session.get('user_id'):
         return render(request, 'availability/error.html')
     if request.method == 'POST':
         villa = get_object_or_404(Villa , villa_id = villa_id)
         year = int(request.POST.get('year'))
         month = int(request.POST.get('month'))
-        j_days = {1: 31, 2: 31, 3: 31, 4: 31, 5: 31, 6: 31, 7: 30, 8: 30, 9: 30, 10: 30, 11: 30, 12: 29}
         if month == 12 and jdatetime.date(year, 12, 1).isleap():
             days = 30
         else:
@@ -28,7 +29,7 @@ def update_availability(request, villa_id):
             d, created = Availability.objects.get_or_create(villa= villa, date= date)
             d.status = status
             d.save()
-        return redirect('availability:update_availability', villa_id=villa_id )
+        return redirect('Villas:host_dashboard')
     return render(request, 'availability/update_availability.html', {
             "villa": villa,
             "current_year": jdatetime.date.today().year,
