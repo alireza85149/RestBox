c704805718d69b498b2df3974faa20d999473f48
alireza <mohammadianalireza85149@gmail.com>
2026-07-16 11:25:34 +0330

    the payments.models.Payment added

--- RestBox/payments/models.py
+++ RestBox/payments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from reservations.models import Reservation
+class Payment(models.Model):
+    payment_id = models.AutoField(primary_key=True)
+    reservation = models.OneToOneField(Reservation,on_delete=models.CASCADE,related_name="payment")
+    amount = models.IntegerField()
+    status = models.CharField(max_length=10,choices=[("pending", "Pending"),("success", "Success"),("failed", "Failed"),],default="pending")
+    gateway_ref = models.CharField(max_length=100,blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"Payment #{self.payment_id}"
